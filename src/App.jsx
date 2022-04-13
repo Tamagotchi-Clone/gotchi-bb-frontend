bdab6072c49a4ceec0748a701136db16a8aa8237
Violet Stewart <vkazoo2@gmail.com>
2022-04-13 10:28:48 -0700

    set up file structure and routes in App js

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,28 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Pet from './views/Pet/Pet';
+import Home from './views/Home/Home';
+import Profile from './views/Profile/Profile';
+import ChoosePet from './views/ChoosePet/ChoosePet';
+
 export default function App() {
-  return <h1>Gatchi</h1>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/profile">
+            <Profile />
+          </Route>
+          <Route exact path="/choosepet">
+            <ChoosePet />
+          </Route>
+          <Route exact path="/pet">
+            <Pet />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
---
+++ src/views/ChoosePet/ChoosePet.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ChoosePet() {
+  return (
+    <div>
+      <h1>Choose your pet</h1>
+    </div>
+  );
+}
