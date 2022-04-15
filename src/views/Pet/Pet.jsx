d4b7e1b8990d0250c33b8785b3940c509fdd7a6b
Ian Christiansen <ianc.songdog@gmail.com>
2022-04-15 12:12:56 -0700

    feature: Pet page
    added useParams for fetchpet; pet data successfully fetched by id

--- src/views/Pet/Pet.jsx
+++ src/views/Pet/Pet.jsx
@@ -1,15 +1,18 @@
 import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import PetPage from '../../components/Pet/PetPage';
 import { getUserPetById } from '../../services/userpets';
 
 export default function Pet() {
   const [pet, setPet] = useState({});
   const [loading, setLoading] = useState(true);
+  const params = useParams();
 
   useEffect(() => {
     const fetchPet = async (id) => {
-      const data = await getUserPetById(id);
+      const data = await getUserPetById(params.id);
       setPet(data);
+      console.log('data', data);
       setLoading(false);
     };
     fetchPet();
