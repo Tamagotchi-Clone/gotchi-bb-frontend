3301b63fca5fbd3ef2cfbf7d47b7f741424fc121
Ian Christiansen <ianc.songdog@gmail.com>
2022-04-15 12:18:48 -0700

    adding structure to petpage component

--- src/components/Pet/PetPage.js
+++ src/components/Pet/PetPage.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import Pet from '../../views/Pet/Pet';
 
-export default function Petpage() {
-  return <div>Pet</div>;
+export default function Petpage({ pet }) {
+  return (
+    <>
+      <div>
+        <h1>{pet.name}</h1>
+      </div>
+      <button>feed</button>
+      <button>play</button>
+      <button>clean</button>
+    </>
+  );
 }
