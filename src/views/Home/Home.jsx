caddb385f8c66a7179303d78a0359adc68249155
Ian Christiansen <ianc.songdog@gmail.com>
2022-04-20 10:18:37 -0700

    troubleshooting homepage

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import 'animate.css';
+import { getPets } from '../../services/pets';
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+  const [randomImage, setRandomImage] = useState([{}]);
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      const data = await getPets();
+      const randomData = data[Math.floor(Math.random() * data.length)];
+      console.log(randomData.image);
+      setRandomImage(randomData.image);
+      setLoading(false);
+    };
+    if (loading) {
+      fetchImage();
+    }
+  }, [loading]);
+
   return (
     <div>
-      <h1>Welcome to Gotchi</h1>
+      <h1 className="animate__animated animate__flash">Welcome to gotchi bb</h1>
+      <img
+        className="animate__animated animate__bounce"
+        src={randomImage}
+      ></img>
     </div>
   );
 }
