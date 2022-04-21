9df75f6b855bdef78936c2edf87171c48355f339
Ian Christiansen <ianc.songdog@gmail.com>
2022-04-20 17:17:40 -0700

    style changes

--- src/components/Pet/PetPage.js
+++ src/components/Pet/PetPage.js
@@ -16,25 +16,29 @@ export default function Petpage({
         <h1>{pet.name}</h1>
         <div className="petScreen">
           <img src={pet.image} />
+          <hr />
         </div>
         <div className="petStats">
           <div className="stat-container">
             <img src="https://i.postimg.cc/Zv8mtdL7/status-bar.png" />
             hunger
-            <button onClick={handleFeed}>Feed</button>
+            {/* <button onClick={handleFeed}>Feed</button> */}
           </div>
           <div className="stat-container">
             <img src="https://i.postimg.cc/VJdC1f39/status3.png" />
             hygiene
-            <button onClick={handleClean}>Clean</button>
+            {/* <button onClick={handleClean}>Clean</button> */}
           </div>
           <div className="stat-container">
             <img src="https://i.postimg.cc/MvsQhMrK/status2.png" />
             entertainment
-            <button onClick={handlePlay}>Play</button>
           </div>
         </div>
-        <div className="buttons"></div>
+      </div>
+      <div className="buttons">
+        <button onClick={handleFeed}>Feed</button>
+        <button onClick={handleClean}>Clean</button>
+        <button onClick={handlePlay}>Play</button>
       </div>
     </>
   );
