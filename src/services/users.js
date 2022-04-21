1ee4a38e31e49b8068a41029e8c8bfec66fa2248
ckaynatividad <natividad.ckay@gmail.com>
2022-04-21 14:10:00 -0700

    test

--- src/services/users.js
+++ src/services/users.js
@@ -11,7 +11,7 @@ export async function signIn({ username, password }) {
     const resJson = await res.json();
     return resJson;
   } catch (error) {
-    throw new Error();
+    return null;
   }
 }
 
@@ -28,7 +28,7 @@ export async function signUp({ username, password }) {
     const resJson = await res.json();
     return resJson;
   } catch (error) {
-    throw new Error();
+    return null;
   }
 }
 
@@ -41,7 +41,7 @@ export async function getCurrentUser() {
     const resJson = await res.json();
     return resJson;
   } catch (error) {
-    throw error;
+    return null;
   }
 }
 
