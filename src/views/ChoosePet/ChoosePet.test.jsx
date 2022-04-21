bc303d4602eaee67cd43577a62753512b5affba4
ckaynatividad <natividad.ckay@gmail.com>
2022-04-21 12:56:27 -0700

    more msw

--- src/views/ChoosePet/ChoosePet.test.jsx
+++ src/views/ChoosePet/ChoosePet.test.jsx
@@ -24,13 +24,28 @@ const data = [
   },
   { id: '3', species: 'Dino', image: 'https://i.postimg.cc/bGkYHhRQ/pet3.png' },
 ];
+const pet = {
+  cleanliness: '2022-04-21T17:51:50.940Z',
+  hunger: '2022-04-21T17:51:47.178Z',
+  id: '3',
+  name: 'd',
+  petId: '1',
+  play: '2022-04-21T17:51:49.195Z',
+  userId: '1',
+};
 const server = setupServer(
   rest.get(`${process.env.API_URL}/api/v1/pets`, (req, res, ctx) => {
     return res(ctx.json(data));
   }),
-  rest.get('http://localhost:7890/api/v1/users/me', (req, res, ctx) => {
+  rest.get(`${process.env.API_URL}/api/v1/users/me`, (req, res, ctx) => {
     return res(ctx.json(mockUser));
-  })
+  }),
+  rest.get(
+    `${process.env.API_URL}/api/v1/userpets/users/1`,
+    (req, res, ctx) => {
+      return res(ctx.json(pet));
+    }
+  )
 );
 beforeAll(() => server.listen());
 afterAll(() => server.close());
