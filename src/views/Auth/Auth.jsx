574cb5e7b216bb63982f95730059e3644b471654
Ian Christiansen <ianc.songdog@gmail.com>
2022-04-21 17:15:07 -0700

    added fadein to auth form

--- src/views/Auth/Auth.jsx
+++ src/views/Auth/Auth.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import AuthForm from '../../components/Auth/AuthForm';
 import '../../components/Auth/AuthForm.css';
+import FadeIn from 'react-fade-in/lib/FadeIn';
 
 export default function Auth({ isRegistering = false }) {
   return (
-    <div className="authBox">
-      <AuthForm isRegistering={isRegistering} />
-    </div>
+    <FadeIn>
+      <div className="authBox">
+        <AuthForm isRegistering={isRegistering} />
+      </div>
+    </FadeIn>
   );
 }
